96e69585ed98ae0f531638122ded92b4a8572636
chris-alory <christopher@alory.fr>
2022-08-03 15:32:42 +1000

    Template nav (desktop & mobile)

---
+++ ressources/js/script.js
@@ -0,0 +1,14 @@
+
+function openNav() {
+
+    let toggler = document.querySelector('.menu-mobile');
+
+    let navigation = document.querySelector('.mobile-nav');
+
+    toggler.onclick = function () {
+        navigation.classList.toggle('open');
+    }
+
+}
+
+openNav();
